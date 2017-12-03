af4c30c8ffd36c5e1e87dfcc2d57b9d8cbcb070c
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2017-12-03 12:15:34 +0200

    Update sample code

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ fn main() {
     println!("Hello, WebAssembly!");
 }
 
-// Functions that you wish to access from Javascript
-// must be marked as no_mangle
+// Do not apply the standard name mangling. Set the symbol for this item to its identifier.
+// The no_mangle attribute turns off Rust's name mangling, so that it is easier to link to.
+// Functions that you wish to access from Javascript must be marked as no_mangle.
 #[no_mangle]
 pub fn add_two_numbers(x: i32, y: i32) -> i32 {
     x + y
