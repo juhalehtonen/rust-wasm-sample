d2206c34068adf09157f9b7e7aff54ad5442db69
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2017-12-03 12:02:17 +0200

    Update example code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    println!("Hello, WebAssembly");
+    println!("Hello, WebAssembly!");
 }
 
 // Functions that you wish to access from Javascript
 // must be marked as no_mangle
 #[no_mangle]
-pub fn add_twenty_seven(n: i32) -> i32 {
-    n + 27
+pub fn add_two_numbers(x: i32, y: i32) -> i32 {
+    x + y
 }
